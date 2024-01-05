c51cbd972aa2798e2cfd7987cfd8c58f07698734
PGAlmeida <pedro8tp@gmail.com>
2024-01-05 09:43:38 -0300

    consolo.table

---
+++ table.js
@@ -0,0 +1,16 @@
+let diario = {
+    pessoa1: {
+        nome: "Pedro",
+        idade: 19
+    },
+    pessoa2: {
+        nome: "João",
+        idade: 63
+    },
+    pessoa3: {
+        nome: "Julia",
+        idade: 25
+    },
+}
+
+console.table(diario)
